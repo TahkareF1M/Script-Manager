22652d6c926d043f09bb1105b11bc52f31b1b9f7
TahkareF1M <112979562+TahkareF1M@users.noreply.github.com>
2022-09-06 23:38:41 +0200

    Added support for arguments
    It is now possible to pass a string as an argument to a script.

--- script_manager.py
+++ script_manager.py
@@ -30,9 +30,11 @@ def run_script():
     script = chosen_script_entry.get()
     save = chosen_save_entry.get()
     if len(script) > 0 and len(save) > 0:
-        command = "python scripts/" + script + " --input " + save
         process_unpack(save, "scripts/result")
-        script_map[script].run_script()
+        try:
+            script_map[script].run_script(option_entry.get())
+        except:
+            script_map[script].run_script()
         process_repack("scripts/result", save)
         run_label_var.set("Ran " + script + " on " + save)
         log.write("[" + str(datetime.now()) + "] Execution - Script : " + script + " - Savefile : " + save + "\n")
@@ -59,28 +61,28 @@ window.title("F1 Manager 22 Script Manager")
 window.columnconfigure(0)
 window.columnconfigure(1)
 window.columnconfigure(2, weight=5)
-window.geometry('750x440')
+window.geometry('770x430')
 window.resizable(False, False)
 font = tkFont.Font(family="Sans Serif", size=12)
 font_small = tkFont.Font(family="Sans Serif", size=10)
 
-LIST_HEIGHT = 20
+LIST_HEIGHT = 22
 
 script_label = tk.Label(master=window, text="Script list", font=font)
 script_label.grid(column=0, row=0, rowspan=1, padx=10, pady=5)
-script_listbox = tk.Listbox(master=window, height=LIST_HEIGHT, listvariable=tk.StringVar(value=scripts), font=font)
+script_listbox = tk.Listbox(master=window, height=LIST_HEIGHT, width=25, listvariable=tk.StringVar(value=scripts), font=font_small)
 script_listbox.grid(column=0, row=1, rowspan=LIST_HEIGHT, padx=10, pady=5)
 script_listbox.bind('<<ListboxSelect>>', script_select)
 
 save_label = tk.Label(master=window, text="Savefile list", font=font)
 save_label.grid(column=1, row=0, rowspan=1, padx=10, pady=5)
-save_listbox = tk.Listbox(master=window, height=LIST_HEIGHT, listvariable=tk.StringVar(value=saves), font=font)
+save_listbox = tk.Listbox(master=window, height=LIST_HEIGHT, width=25, listvariable=tk.StringVar(value=saves), font=font_small)
 save_listbox.grid(column=1, row=1, rowspan=LIST_HEIGHT, padx=10, pady=5)
 save_listbox.bind('<<ListboxSelect>>', save_select)
 
 chosen_script_label = tk.Label(master=window, text="Selected script :", font=font)
 chosen_script_label.grid(column=2, row=1, padx=10, pady=0)
-chosen_script_entry = tk.Entry(master=window, width=45, font=font)
+chosen_script_entry = tk.Entry(master=window, width=45, font=font_small)
 chosen_script_entry.grid(column=2, row=2, padx=10, pady=0)
 chosen_script_entry.configure(state='disabled')
 
@@ -91,16 +93,21 @@ desc_script_text.grid(column=2, row=5, rowspan=6, padx=10, pady=0)
 desc_script_text.configure(state='disabled')
 
 chosen_save_label = tk.Label(master=window, text="Selected savefile :", font=font)
-chosen_save_label.grid(column=2, row=14, padx=10, pady=0)
-chosen_save_entry = tk.Entry(master=window, width=45, font=font)
-chosen_save_entry.grid(column=2, row=15, padx=10, pady=0)
+chosen_save_label.grid(column=2, row=12, padx=10, pady=0)
+chosen_save_entry = tk.Entry(master=window, width=45, font=font_small)
+chosen_save_entry.grid(column=2, row=13, padx=10, pady=0)
 chosen_save_entry.configure(state='disabled')
 
+option_label = tk.Label(master=window, text="Script argument (if needed) :", font=font)
+option_label.grid(column=2, row=15, padx=10, pady=0)
+option_entry = tk.Entry(master=window, width=45, font=font_small)
+option_entry.grid(column=2, row=16, padx=10, pady=0)
+
 run_button = tk.Button(master=window, text="Run script", command=run_script, font=font)
-run_button.grid(column=2, row=17, padx=10, pady=0)
+run_button.grid(column=2, row=18, padx=10, pady=0)
 run_label_var = tk.StringVar()
 run_label = tk.Label(master=window, textvariable=run_label_var, width=45, font=font_small)
-run_label.grid(column=2, row=18, padx=10, pady=0)
+run_label.grid(column=2, row=19, padx=10, pady=0)
 
 window.mainloop()
 
